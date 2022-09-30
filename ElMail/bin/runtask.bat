f737a625f91b003082697c580ae9dfd83cb2d233
Gregor Kappler <mail@g-kappler.de>
2022-09-30 10:26:12 +0200

    Elmail Genie App, generated

---
+++ ElMail/bin/runtask.bat
@@ -0,0 +1,1 @@
+"/home/gregor/julia-1.8.1/bin/julia" --color=yes --depwarn=no --project=@. -q -- "%~dp0..\bootstrap.jl" -r %*
--- config/secrets.jl
+++
@@ -1,1 +0,0 @@
-Genie.secret_token!("0f0f6649b983cf6b6279f2d7d68a046ce0ca03d6827b41c04fb811fc7ffa4f60") 
