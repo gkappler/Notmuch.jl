48870e03bee1a928be93ea333ba2a225e114035e
Gregor Kappler <mail@g-kappler.de>
2022-10-02 11:46:16 +0200

    genie config 999, routes
    genie config 999, routes

--- ElMail/config/env/dev.jl
+++ ElMail/config/env/dev.jl
@@ -1,15 +1,18 @@
 using Genie, Logging
 
 Genie.Configuration.config!(
-  server_port                     = 8000,
-  server_host                     = "127.0.0.1",
-  log_level                       = Logging.Info,
-  log_to_file                     = false,
-  server_handle_static_files      = true,
-  path_build                      = "build",
-  format_julia_builds             = true,
-  format_html_output              = true,
-  watch                           = true
+    server_port                     = 9999,
+    server_host                     = "0.0.0.0",
+    log_level                       = Logging.Debug,
+    log_to_file                     = true,
+    server_handle_static_files      = true,
+    path_build                      = "build",
+    format_julia_builds             = true,
+    format_html_output              = true,
+    cors_headers                    = Dict(
+        "Access-Control-Allow-Origin" => "*",
+        "Access-Control-Allow-Headers" => "Content-Type",
+        "Access-Control-Allow-Methods" => "GET,POST")
 )
 
 ENV["JULIA_REVISE"] = "auto"
