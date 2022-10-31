43b2a6bf38647c0f1a921351af300e4fe6f07fab
Gregor Kappler <mail@g-kappler.de>
2022-10-31 17:47:21 +0100

    docker use of ElMail sub-package

--- ElMail/bootstrap.jl
+++ ElMail/bootstrap.jl
@@ -1,5 +1,11 @@
 (pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
+using Pkg
+Pkg.activate(".");
+Pkg.add(path="..");
+Pkg.add(url="https://github.com/gkappler/SMTPClient.jl");
+Pkg.instantiate();
+
 using ElMail
 const UserApp = ElMail
 ElMail.main()
